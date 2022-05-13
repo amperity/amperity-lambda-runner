7e321f5e270f8affba57fd91ec3da2b280d724ee
Phil Winchester <philipwinc@gmail.com>
2022-05-13 12:32:11 -0400

    add simple flask app for easier local development

---
+++ destinations/destination_app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/health')
+def health_check():
+    print('Checking Health')
+    return jsonify(message="up", status=200)
+
+
+@app.route("/mock/rudderstack", methods=['POST'])
+def mock_rudderstack():
+    auth_header = request.headers.get('Authorization', 'Basic dummy_id')
+    req_id = auth_header.split(' ')[1]
+    req = request.json
+    print(req)
+
+    for rec in req.get('batch'):
+        if 'userId' not in rec:
+            print(rec)
+            # TODO return actual 400 w/ this message
+            return "Invalid request. Check destination_app logs"
+    
+    return "Check destination_app logs"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5005)
