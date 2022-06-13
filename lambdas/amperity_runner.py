f26e90ea90259d75f48fdc9f3b5fd458763f01a8
Andy Vo <andyvo@ip-192-168-7-199.us-west-2.compute.internal>
2022-06-13 10:36:44 -0700

    work in progress

--- lambdas/amperity_runner.py
+++ lambdas/amperity_runner.py
@@ -2,8 +2,7 @@ import json
 import requests
 
 class AmperityRunner:
-    def __init__(self, payload, lambda_context, destination_url, destination_session, 
-                    batch_size=3500, batch_offset=0, rate_limit=None, custom_mapping=None):
+    def __init__(self, payload, lambda_context, batch_size=3500, batch_offset=0, rate_limit=None, custom_mapping=None, read_ndjson = False):
         """
         :params
             payload : str
@@ -32,8 +31,6 @@ class AmperityRunner:
         """
 
         self.lambda_context = lambda_context
-        self.destination_url = destination_url
-        self.destination_session = destination_session
         self.batch_size = batch_size
         self.batch_offset = batch_offset
         self.rate_limit = rate_limit
@@ -41,16 +38,16 @@ class AmperityRunner:
         self.rate_limit_time_start = None
         self.custom_mapping = custom_mapping
 
-        self.data_url = self.payload.get("data_url")
-        self.webhook_settings = self.payload.get("webhook_settings")
-        self.callback_url = self.payload.get("callback_url")
-        self.webhook_id = self.payload.get("webhook_id")
-        self.access_token = self.payload.get("access_token")
+        self.data_url = payload.get("data_url")
+        self.webhook_settings = payload.get("webhook_settings")
+        self.callback_url = payload.get("callback_url")
+        self.webhook_id = payload.get("webhook_id")
+        self.access_token = payload.get("access_token")
 
         self.status_url = self.callback_url + self.webhook_id
-
     
     def download_file(self, url):
+        print("Downloading file.")
         res = requests.get(url)
         if res.status_code != 200:
             print("Could not download file.")
@@ -68,10 +65,23 @@ class AmperityRunner:
             "progress": progress,
             "errors": errors})
         res = requests.put(self.status_url, headers = headers, data = data)
-        print(res)
+        print("poll for status", res)
         # add some error handling, or handle this response somehow?
     
-    def run(self, callback):
+    def read_ndjson(self, data, batch_offset, batch_size):
+        """
+        The thought here is to allow the user to set
+        whether or not they read the NDJSON within the run function.
+
+        Some users may want to process the entire file instead of splitting
+        into batches.
+        """
+        data_array = data.decode('utf-8').splitlines()
+        batch = data_array[self.batch_offset: self.batch_offset + self.batch_size]
+        data_batch = [json.loads(d) for d in batch]
+        return data_batch
+    
+    def run(self, callback, *args):
         errors = []
         self.poll_for_status("running", 0, errors)
         downloaded_file = self.download_file(self.data_url)
@@ -79,8 +89,13 @@ class AmperityRunner:
             errors.append("Could not download file.")
             self.poll_for_status("failed", 0, errors)
             return False
+        
+        if self.read_ndjson == True:
+            data_batch = self.read_ndjson(downloaded_file, self.batch_offset, self.batch_size)
+            res = callback(data_batch, errors, *args)
+        else:
+            res = callback(downloaded_file, errors, *args)
 
-        res = callback(errors)
         if not res:
             errors.append("Error running function.")
             self.poll_for_status("failed", 0, errors)
