e2cdec4fc79026e2a82f29185550a31edd1a1afb
Andy Vo <andyvo@ip-192-168-1-165.us-west-2.compute.internal>
2022-06-15 10:48:13 -0700

    added template

--- lambdas/lambda_handlers/template.py
+++ lambdas/lambda_handlers/template.py
@@ -1,27 +1,16 @@
-import requests
-import sys
-sys.path.append("../")
+import json
+
 from amperity_runner import AmperityRunner
+from response import http_response
 
-
-def main_function(data, errors, url, args):
-    print("main_function data", data)
-    print("main_function args", args)
-    # res = requests.get(url)
-    print("errors from main_function", errors)
+def main(data):
+    # DO STUFF
+    return http_response(200, "SUCCESS", "Successfully ran function")
 
 def lambda_handler(event, context):
-    url = event["data_url"]
-    amperity = AmperityRunner(payload = event, lambda_context = context, read_ndjson = False)
-    amperity.run(main_function, url, "hi")
-
-payload = {
-    "data_url": "https://www.google.com",
-    "webhook_settings": {},
-    "callback_url": "https://www.google.com/",
-    "webhook_id": "webhook_id",
-    "access_token": "access_token"
-    }
-lambda_context = {"context": "hi"}
-
-lambda_handler(payload, lambda_context)
+    print(event)
+    payload = json.loads(event["body"])
+    amperity = AmperityRunner(payload = payload, lambda_context = context, read_as_ndjson = True)
+    res = amperity.run(main)
+   
+    return res
